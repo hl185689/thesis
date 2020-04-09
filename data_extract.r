178a42bc3dddacf1593492742edeef58ae97fd34
conor <conor@CFCSPATIAL-LW18.gs.umt.edu>
2020-04-09 12:46:00 -0600

    loading other R code

---
+++ data_extract.r
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(dbplyr)
+library(DBI)
+library(scales)
+library(lubridate)
+library(dbplyr)
+
+working.dir <- "C:\\Users\\jchan\\Dropbox\\Research\\LeonardHannah\\"
+
+con <- dbConnect(RSQLite::SQLite(),
+                 dbname=paste0(working.dir,
+                               "targetsmart_data.db"))
+
+dbListTables(con)
+
+tsdata.1 <- tbl(con, "TSDATA_1_500")
+tsdata.2 <- tbl(con,"TSDATA_501_1000")
+
+d.1 <- tsdata.1 %>% 
+  select(voterbase_id,
+         "vb.tsmart_first_name",
+         "vb.tsmart_last_name",
+         "vb.tsmart_full_address",
+         "vb.tsmart_zip") %>% 
+  collect()
+
+d.2 <- tsdata.2 %>% 
+  select(voterbase_id,
+         contains("farm")) %>% 
+  collect()
+
+d <- d.1 %>% 
+  left_join(d.2,by="voterbase_id")
+
+d %>% 
+  filter(vb.tsmart_last_name == "MOHR",
+         vb.tsmart_first_name == "JACQUELINE") %>% 
+  data.frame
