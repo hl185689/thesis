e8000c7ac0fcdfdac794df14a49cbfb6e5e4bfc0
John Chandler <john.chandler@gmail.com>
2020-05-26 12:12:22 -0500

    removing top-level files

--- null_q.R
+++
@@ -1,5 +0,0 @@
-working.dir <- "C:\\Users\\hl185689\\Documents\\Thesis\\Data\\tsmart_um_mt_analytic_20200210\\"
-d <- readr::read_tsv(paste0(working.dir,"tsmart_um_mt_analytic_20200210.csv"))
-
-
-colSums(is.na(d))
